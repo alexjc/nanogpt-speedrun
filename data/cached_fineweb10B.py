c4481c6e18c4cd22d97d1fea7566719f0cbe090b
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2025-01-22 20:46:07 +0100

    SpeedRun for GPT-2 medium.

--- data/cached_fineweb10B.py
+++ data/cached_fineweb10B.py
@@ -8,7 +8,7 @@ def get(fname):
     if not os.path.exists(os.path.join(local_dir, fname)):
         hf_hub_download(repo_id="alexjc/fineweb-tokmon-10B", filename=fname,
                         repo_type="dataset", local_dir=local_dir)
-get("english-28416-balanced/fineweb-tokmon_val_%06d.bin" % 0)
+get("english-28416-balanced-v1/fineweb-tokmon_val_%06d.bin" % 0)
 
 num_chunks = 101
 
@@ -20,7 +20,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 with ThreadPoolExecutor(max_workers=8) as executor:
     futures = [
-        executor.submit(get, f"english-28416-balanced/fineweb-tokmon_train_{i:06d}.bin")
+        executor.submit(get, f"english-28416-balanced-v1/fineweb-tokmon_train_{i:06d}.bin")
         for i in range(1, num_chunks+1)
     ]
     for future in futures:
